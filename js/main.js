f9697ffd677554c61e29d4f35871dedf471b4286
melody200325 <mac@Mac.local>
2017-07-21 10:50:39 -0700

    fixed js bug

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
 	var projectNav = $('#project-nav').offset().top;
 	var designPosition = $('#nao-design').offset().top;
 	var image3Position = $('#nao-design-content-3').offset().top;
-	// var designPosition = $('#trip-design').offset().top;
-	// var image2Position = $('#trip-design-content-2').offset().top;
+	var sap1Position = $('#sap-design').offset().top;
+	var sap2Position = $('#sap-design-content-2').offset().top;
+	var sap3Position = $('#sap-design-content-4').offset().top;
+	var designPosition = $('#trip-design').offset().top;
+	var image2Position = $('#trip-design-content-2').offset().top;
 
 
 // console.log(projectNav);
@@ -46,56 +49,48 @@ $(window).on('scroll', function() {
 
 // project-2 webflow animation
 
+if (distanceScrolled > sap1Position - 600) {
+		$('#sap-image1').addClass('sap-design-content-gallery-animation1');
+		$('#sap-image2').addClass('sap-design-content-gallery-animation1');
+	} else {
+		$('#sap-image1').removeClass('sap-design-content-gallery-animation1');
+		$('#sap-image2').addClass('sap-design-content-gallery-animation1');
+	}
 
-// $(window).on('scroll', function() {
-// 	var distanceScrolled = $(window).scrollTop();
-// 	var sap1Position = $('#sap-design').offset().top;
-// 	var sap2Position = $('#sap-design-content-2').offset().top;
-// 	var sap3Position = $('#sap-design-content-4').offset().top;
-	
+	if (distanceScrolled > sap2Position - 600) {
+		$('#sap-image3').addClass('sap-design-content-gallery-animation1');
+		$('#sap-image4').addClass('sap-design-content-gallery-animation1');
+	} else {
+		$('#sap-image3').removeClass('sap-design-content-gallery-animation1');
+		$('#sap-image4').removeClass('sap-design-content-gallery-animation1');
+	}
+	if (distanceScrolled > sap3Position - 600) {
+		$('#sap-image6').addClass('sap-design-content-gallery-animation1');
+		$('#sap-image7').addClass('sap-design-content-gallery-animation1');
 
-
-// if (distanceScrolled > sap1Position - 600) {
-// 		$('#sap-image1').addClass('sap-design-content-gallery-animation1');
-// 		$('#sap-image2').addClass('sap-design-content-gallery-animation1');
-// 	} else {
-// 		$('#sap-image1').removeClass('sap-design-content-gallery-animation1');
-// 		$('#sap-image2').addClass('sap-design-content-gallery-animation1');
-// 	}
-
-// 	if (distanceScrolled > sap2Position - 600) {
-// 		$('#sap-image3').addClass('sap-design-content-gallery-animation1');
-// 		$('#sap-image4').addClass('sap-design-content-gallery-animation1');
-// 	} else {
-// 		$('#sap-image3').removeClass('sap-design-content-gallery-animation1');
-// 		$('#sap-image4').removeClass('sap-design-content-gallery-animation1');
-// 	}
-// 	if (distanceScrolled > sap3Position - 600) {
-// 		$('#sap-image6').addClass('sap-design-content-gallery-animation1');
-// 		$('#sap-image7').addClass('sap-design-content-gallery-animation1');
-
-// 	} else {
-// 		$('#sap-image6').removeClass('sap-design-content-gallery-animation1');
-// 		$('#sap-image7').removeClass('sap-design-content-gallery-animation1');
-// 	}
-// }); 
+	} else {
+		$('#sap-image6').removeClass('sap-design-content-gallery-animation1');
+		$('#sap-image7').removeClass('sap-design-content-gallery-animation1');
+	}
+}); 
 
 
-// // project-3 webflow animation
+// project-3 webflow animation
 
 	
-// 	if (distanceScrolled > designPosition - 600) {
-// 		$('#trip-image1').addClass('trip-design-content-gallery-animation');
-// 	} else {
-// 		$('#trip-image1').removeClass('trip-design-content-gallery-animation');
-// 	}
+	if (distanceScrolled > designPosition - 600) {
+		$('#trip-image1').addClass('trip-design-content-gallery-animation');
+	} else {
+		$('#trip-image1').removeClass('trip-design-content-gallery-animation');
+	}
 
-// 	if (distanceScrolled > image2Position - 600) {
-// 		$('#trip-image2').addClass('trip-design-content-gallery-animation');
-// 	} else {
-// 		$('#trip-image2').removeClass('trip-design-content-gallery-animation');
-// 	}
+	if (distanceScrolled > image2Position - 600) {
+		$('#trip-image2').addClass('trip-design-content-gallery-animation');
+	} else {
+		$('#trip-image2').removeClass('trip-design-content-gallery-animation');
+	}
 
+}); 
 }); 
 
 
