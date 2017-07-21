1a06f0d00dfbaa9c4347d98daca64e165e579cc5
melody200325 <mac@Mac.local>
2017-07-21 12:04:21 -0700

    competed project-3 animation

--- js/project-3.js
+++ js/project-3.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	var projectNav = $('#project-nav').offset().top;
 	var designPosition = $('#trip-design').offset().top;
 	var image2Position = $('#trip-design-content-2').offset().top;
+	var image4Position = $('#trip-design-content-4').offset().top;
+	var image5Position = $('#trip-design-content-5').offset().top;
 
 
 // console.log(projectNav);
@@ -39,6 +41,17 @@ $(window).on('scroll', function() {
 		$('#trip-image2').addClass('trip-design-content-gallery-animation');
 	} else {
 		$('#trip-image2').removeClass('trip-design-content-gallery-animation');
+	}
+	if (distanceScrolled > image4Position - 600) {
+		$('#trip-image4').addClass('trip-design-content-gallery-animation');
+	} else {
+		$('#trip-image4').removeClass('trip-design-content-gallery-animation');
+	}
+
+	if (distanceScrolled > image5Position - 600) {
+		$('#trip-image5').addClass('trip-design-content-gallery-animation');
+	} else {
+		$('#trip-image5').removeClass('trip-design-content-gallery-animation');
 	}
 
 }); 
